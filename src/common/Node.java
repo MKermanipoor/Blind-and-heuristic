a0046f4e70c9139d9048d5b6e07fd7e1462c45ec
MKP <masoud.kermanipoor@gmail.com>
2018-12-29 10:48:34 +0330

    complete common codes without heuristic

--- src/common/Node.java
+++ src/common/Node.java
@@ -1,16 +1,75 @@
 package common;
 
+import javafx.util.Pair;
+
 import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
 public class Node {
     private State state;
     private Node parent;
+    private int cost = 0;
 
-//    public List<State> soccesoreFunction(){
-//        List<State> result = new ArrayList<>();
-//
-//    }
+    public enum Action{
+        UP,DOWN,
+        LEFT,RIGHT
+    }
+
+    public Node(State state) {
+        this.state = state;
+    }
+
+    public List<Node> soccesoreFunction(){
+        List<Node> result = new ArrayList<>();
+        Node temp;
+
+        //up
+        temp = this.doAction(Action.UP);
+        if (temp != null)
+            result.add(temp);
+
+        //down
+        temp = this.doAction(Action.DOWN);
+        if (temp != null)
+            result.add(temp);
+
+        //left
+        temp = this.doAction(Action.LEFT);
+        if (temp != null)
+            result.add(temp);
+
+        //right
+        temp = this.doAction(Action.RIGHT);
+        if (temp != null)
+            result.add(temp);
+
+        return result;
+    }
+
+    private Node doAction(Action act){
+        Pair<State, Integer> pair = null;
+        switch (act){
+            case UP:
+                 pair = this.state.up();
+                break;
+            case DOWN:
+                pair = this.state.down();
+                break;
+            case LEFT:
+                pair = this.state.left();
+                break;
+            case RIGHT:
+                pair = this.state.right();
+                break;
+        }
+        if (pair == null)
+            return null;
+        Node result = new Node(pair.getKey());
+        result.parent = this;
+        result.cost = this.cost + pair.getValue();
+        return result;
+    }
 
     @Override
     public boolean equals(Object obj) {
@@ -18,6 +77,17 @@ public class Node {
             return super.equals(obj);
 
         Node n = (Node) obj;
-        return n.state.equals(this.state);
+        if (n.cost>this.cost)
+            return n.state.equals(this.state);
+        return false;
+    }
+
+    @Override
+    public String toString() {
+        return state.toString() + cost;
+    }
+
+    public static Node getInput(){
+        return new Node(State.getFromInput());
     }
 }
--- src/common/State.java
+++ src/common/State.java
@@ -1,19 +1,35 @@
 package common;
 
-import static common.PublicValue.*;
+import javafx.util.Pair;
+
+import static common.PublicValue.PUZZLE_SIZE;
+import static common.PublicValue.scanner;
 
 public class State {
+    static {
+        int[][] temp = new int[PUZZLE_SIZE][PUZZLE_SIZE];
+        int cont = 1;
+        for (int i = 0; i < PUZZLE_SIZE; i++) {
+            for (int j = 0; j < PUZZLE_SIZE; j++) {
+                temp[i][j] = cont++;
+            }
+        }
+        arrayCode = temp;
+    }
+
+    private static final int[][] arrayCode;
+
     private int[][] puzzle = new int[PUZZLE_SIZE][PUZZLE_SIZE];
     private int freeRow;
     private int freeColumn;
 
     public State(int[][] puzzle) {
-        for (int i = 0; i < PUZZLE_SIZE ; i++) {
-            for (int j = 0; j < PUZZLE_SIZE ; j++) {
+        for (int i = 0; i < PUZZLE_SIZE; i++) {
+            for (int j = 0; j < PUZZLE_SIZE; j++) {
                 this.puzzle[i][j] = puzzle[i][j];
                 if (puzzle[i][j] == 0) {
-                    freeRow = j;
-                    freeColumn = i;
+                    freeColumn = j;
+                    freeRow = i;
                 }
             }
         }
@@ -25,8 +41,8 @@ public class State {
             return super.equals(obj);
 
         int[][] a = ((State) obj).puzzle;
-        for (int i = 0; i < PUZZLE_SIZE ; i++) {
-            for (int j = 0; j < PUZZLE_SIZE ; j++) {
+        for (int i = 0; i < PUZZLE_SIZE; i++) {
+            for (int j = 0; j < PUZZLE_SIZE; j++) {
                 if (puzzle[i][j] != a[i][j])
                     return false;
             }
@@ -34,38 +50,76 @@ public class State {
         return true;
     }
 
-    private State up() {
+    public Pair<State, Integer> up() {
+        if (freeRow == PUZZLE_SIZE - 1)
+            return null;
+
+        State result = new State(this.puzzle);
+
+        result.puzzle[result.freeRow][result.freeColumn] = result.puzzle[result.freeRow + 1][result.freeColumn];
+        int cost = Math.abs(arrayCode[result.freeRow][result.freeColumn] - result.puzzle[freeRow][freeColumn]);
+        result.puzzle[++result.freeRow][result.freeColumn] = 0;
+
+        return new Pair<>(result, cost);
+    }
+
+    public Pair<State, Integer> down() {
+        if (freeRow == 0)
+            return null;
+
+        State result = new State(this.puzzle);
+
+        result.puzzle[result.freeRow][result.freeColumn] = result.puzzle[result.freeRow - 1][result.freeColumn];
+        int cost = Math.abs(arrayCode[result.freeRow][result.freeColumn] - result.puzzle[freeRow][freeColumn]);
+        result.puzzle[--result.freeRow][result.freeColumn] = 0;
+
+        return new Pair<>(result, cost);
+    }
+
+    public Pair<State, Integer> left() {
         if (freeColumn == PUZZLE_SIZE - 1)
             return null;
 
         State result = new State(this.puzzle);
 
-        result.puzzle[result.freeColumn][result.freeRow] = result.puzzle[result.freeColumn + 1][result.freeRow];
-        result.puzzle[++result.freeColumn][result.freeRow] = 0;
+        result.puzzle[result.freeRow][result.freeColumn] = result.puzzle[result.freeRow][result.freeColumn + 1];
+        int cost = Math.abs(arrayCode[result.freeRow][result.freeColumn] - result.puzzle[freeRow][freeColumn]);
+        result.puzzle[result.freeRow][++result.freeColumn] = 0;
 
-        return result;
+        return new Pair<>(result, cost);
+
+    }
+
+    public Pair<State, Integer> right() {
+        if (freeColumn == 0)
+            return null;
+
+        State result = new State(this.puzzle);
+
+        result.puzzle[result.freeRow][result.freeColumn] = result.puzzle[result.freeRow][result.freeColumn - 1];
+        int cost = Math.abs(arrayCode[result.freeRow][result.freeColumn] - result.puzzle[freeRow][freeColumn]);
+        result.puzzle[result.freeRow][--result.freeColumn] = 0;
+
+        return new Pair<>(result, cost);
     }
 
     @Override
     public String toString() {
         StringBuilder st = new StringBuilder();
-        for(int i=0 ; i<PUZZLE_SIZE ; i++){
-            for (int j=0 ; j<PUZZLE_SIZE ; j++)
+        for (int i = 0; i < PUZZLE_SIZE; i++) {
+            for (int j = 0; j < PUZZLE_SIZE; j++)
                 st.append(puzzle[i][j] + "\t");
             st.append("\n");
         }
         return st.toString();
     }
 
-    public static void main(String[] arg){
+    public static State getFromInput() {
         int[][] p = new int[PUZZLE_SIZE][PUZZLE_SIZE];
-        for (int i = 0 ; i<PUZZLE_SIZE ; i++)
-            for (int j=0 ; j<PUZZLE_SIZE ; j++)
+        for (int i = 0; i < PUZZLE_SIZE; i++)
+            for (int j = 0; j < PUZZLE_SIZE; j++)
                 p[i][j] = scanner.nextInt();
 
-        State s = new State(p);
-        System.out.println("*****************\n" + s);
-        System.out.println("*****************\n" + s.up());
-        System.out.println("*****************\n" + s);
+        return new State(p);
     }
 }
